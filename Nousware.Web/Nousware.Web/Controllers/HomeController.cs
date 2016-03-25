f674b39a3f0926e5823d48aa94b38a7439f715dc
Denis <balabanovdenis@gmail.com>
2016-03-25 14:37:44 +0700

    message form

--- Nousware.Web/Nousware.Web/Controllers/HomeController.cs
+++ Nousware.Web/Nousware.Web/Controllers/HomeController.cs
@@ -45,9 +45,38 @@ namespace Nousware.Web.Controllers
             return "fail";
         }
 
-        public ActionResult UserReg(int price)
+        public ActionResult UserReg(int? id)
         {
+            ViewBag.price = id;
             return View();
+        }
+
+        [HttpPost]
+        public string UserReg(string name, string email, string message)
+        {
+            string emailMessage = name + "<br/>" + email + "<br/>" + message;
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.To.Add(ConfigurationManager.AppSettings["Email.ToEmail"]);
+                mailMessage.From = new MailAddress(ConfigurationManager.AppSettings["Email.UserName"]);
+                mailMessage.Subject = "Quick Message(Message from site nous-ware.com)";
+                mailMessage.Body = emailMessage;
+                mailMessage.IsBodyHtml = true;
+
+                string server = ConfigurationManager.AppSettings["Email.Server"];
+                int port = int.Parse(ConfigurationManager.AppSettings["Email.Port"]);
+                SmtpClient smtpClient = new SmtpClient(server, port);
+                smtpClient.EnableSsl = bool.Parse(ConfigurationManager.AppSettings["Email.EnableSsl"]);
+                smtpClient.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["Email.UserName"], ConfigurationManager.AppSettings["Email.Password"]);
+                smtpClient.Send(mailMessage);
+                return "success";
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Could not send the e-mail - error: " + ex.Message);
+            }
+            return "fail";
         }
 
     }
